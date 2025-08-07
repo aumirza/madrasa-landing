ceedba9c2c45b9e273f9806d6f8284ac8e2ca60b
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-08 01:38:06 +0530

    feat: add responsive  styling and structure

--- src/components/HowItWorksSection.tsx
+++ src/components/HowItWorksSection.tsx
@@ -22,21 +22,37 @@ const HOW_IT_WORKS_STEPS = [
 ];
 
 export function HowItWorksSection() {
+  const steps = HOW_IT_WORKS_STEPS.map((item) => (
+    <div className="flex gap-3.5" key={item.title}>
+      {/* Icon Placeholder */}
+      <div className="size-10 shrink-0 rounded-xl bg-accent" />
+
+      {/* Text content */}
+      <div className="flex flex-col gap-1">
+        <p className="font-semibold text-heading">{item.title}</p>
+        <p className="font-manrope text-sm text-subheading">
+          {item.description}
+        </p>
+      </div>
+    </div>
+  ));
   return (
-    <div className="grid h-[21.25rem] grid-cols-2 divide-x">
+    <div className="grid grid-cols-2 divide-x lg:h-[21.25rem]">
       {/*1. Left: CTA section */}
-      <div className="flex h-full flex-col justify-between p-10">
+      <div className="col-span-2 flex h-full flex-1 shrink-0 flex-col justify-between gap-8.5 px-5 py-8.5 sm:p-10 md:col-span-1">
         {/* Top:  Title and description */}
         <div className="space-y-1">
           <p className="font-bold text-2xl text-[#292108]">How it works</p>
-          <p className="font-manrope font-medium text-subheading">
+          <p className="font-manrope font-medium text-sm text-subheading">
             We've built a simpler, faster way to match you with the right Ustaad
             — all streamlined to save you time and effort.
           </p>
         </div>
+        {/*  */}
+        <div className="flex h-full flex-col gap-5 md:hidden">{steps}</div>
         {/* Bottom:  Button with caption */}
         <div className="flex flex-col items-center space-y-3">
-          <p className="font-manrope font-semibold text-sm text-subheading uppercase">
+          <p className="font-manrope font-semibold text-subheading text-xs uppercase">
             Struggling to find a perfect tutor.
           </p>
           <Button
@@ -48,21 +64,8 @@ export function HowItWorksSection() {
         </div>
       </div>
       {/* 2. Right: How it works steps */}
-      <div className="flex h-full flex-col gap-5 p-5">
-        {HOW_IT_WORKS_STEPS.map((item) => (
-          <div className="flex gap-3.5" key={item.title}>
-            {/* Icon Placeholder */}
-            <div className="size-10 shrink-0 rounded-xl bg-accent" />
-
-            {/* Text content */}
-            <div className="flex flex-col gap-1">
-              <p className="font-semibold text-heading">{item.title}</p>
-              <p className="font-manrope text-sm text-subheading">
-                {item.description}
-              </p>
-            </div>
-          </div>
-        ))}
+      <div className="hidden h-full flex-1 shrink-0 flex-col gap-5 p-10 pl-7.5 md:flex md:pl-10 ">
+        {steps}
       </div>
     </div>
   );
