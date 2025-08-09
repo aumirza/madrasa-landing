262454cd1470e6ed0c89bf4a376bb83d811e26bf
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-09 19:47:40 +0530

    feat: add CTAButtons component

---
+++ src/components/CTAButtons.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
+
+export default function CTAButtons({ fullWidth = false }) {
+  return (
+    <div className={`flex ${fullWidth ? 'w-full flex-col' : 'gap-5'}`}>
+      <Button
+        className={cn(
+          fullWidth
+            ? 'w-full rounded-none bg-brand text-primary'
+            : 'hidden rounded-full md:flex'
+        )}
+        size={fullWidth ? 'lg' : 'sm'}
+        type="button"
+        variant={'ghost'}
+      >
+        Join As an Ustaad
+      </Button>
+      <Button
+        className={cn(fullWidth ? 'w-full rounded-none' : 'rounded-full')}
+        size={fullWidth ? 'lg' : 'sm'}
+        type="button"
+      >
+        Get Free Demo
+      </Button>
+    </div>
+  );
+}
