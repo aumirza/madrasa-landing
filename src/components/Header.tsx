f7c5130f478412089a71d0b488d5440fe9fa5138
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-10 01:43:13 +0530

    refactor: adjust layout and spacing in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,22 +12,24 @@ export function Header() {
       className="flex items-center justify-between gap-5 bg-white px-4.5 py-4 shadow-xs md:px-10"
       id="site-header"
     >
-      <div className="flex items-center gap-5">
+      <div className="flex items-center gap-10 md:gap-7">
         <Logo />
-        <Nav />
+        <div className="flex items-center gap-4.5">
+          <Nav />
 
-        <Button
-          asChild
-          className={
-            'group/cta hidden gap-0.5 rounded-full bg-brand px-2.5 py-2 text-primary transition-all duration-200 hover:text-white md:flex'
-          }
-          size="sm"
-        >
-          <Link href="#">
-            Explore Madrasa App
-            <CaretRightIcon className="text-primary transition duration-200 group-hover/cta:text-white" />
-          </Link>
-        </Button>
+          <Button
+            asChild
+            className={
+              'group/cta hidden gap-0.5 rounded-full bg-brand px-2.5 py-2 text-primary transition-all duration-200 hover:text-white md:flex'
+            }
+            size="sm"
+          >
+            <Link href="#">
+              Explore Madrasa App
+              <CaretRightIcon className="text-primary transition duration-200 group-hover/cta:text-white" />
+            </Link>
+          </Button>
+        </div>
       </div>
       <div className="flex items-center gap-3">
         <CTAButtons />
