fa3b8b60f8b0d4b82082bb1edb817b0fb5abe5e4
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-10 01:40:07 +0530

    feat: replace webp logo with svg and update Logo component structure

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
-import madrasaLogo from '@/assets/images/madrasa-logo.webp';
+import Link from 'next/link';
+import madrasaLogo from '@/assets/images/madrasa-logo.svg';
 export default function Logo() {
   return (
-    <Image
-      alt="Madrasa Logo"
-      className="max-h-10 max-w-fit"
-      height={100}
-      src={madrasaLogo}
-      width={100}
-    />
+    <Link className="flex items-center" href="/">
+      <Image
+        alt="Madrasa Logo"
+        className="max-h-10 max-w-fit"
+        height={100}
+        src={madrasaLogo}
+        width={100}
+      />
+    </Link>
   );
 }
