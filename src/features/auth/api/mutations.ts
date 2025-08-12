c87cb48989ccdf051e60a8ab25f9acac93db68cd
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-12 23:01:47 +0530

    feat: add authentication feature along with placeholder API mutations and queries

---
+++ src/features/auth/api/mutations.ts
@@ -0,0 +1,13 @@
+// store mutations here, add a proper type for data to be added according to api
+
+// example mutation
+// export function useLoginMutation() {
+//   return useMutation({
+//     mutationFn:  (data: LoginVariables) => axios.post<LoginResponse>(ENDPOINTS.LOGIN, data),
+//                              use global axios client or scoped axios client as per need
+//     onSuccess: (data) => {
+//         // add to auth store
+//     },
+//   });
+// }
+
