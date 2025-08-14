e0e2813d4ba45446488b80e1bd3456594b558eb0
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-14 12:41:40 +0530

    feat: add new sections on homepage

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,10 @@
 import { AudienceTabs } from '@/components/marketing/AudienceTabs';
 import { ClassOptions } from '@/components/marketing/ClassOptions';
 import { CoursesGrid } from '@/components/marketing/CoursesGrid';
+import { FAQ } from '@/components/marketing/FAQ';
 import { HeroSection } from '@/components/marketing/HeroSection';
+import LatestFromBlog from '@/components/marketing/LatestFromBlog';
+import StuckInDeenBanner from '@/components/marketing/StuckInDeenBannner';
 import { WhyChooseMadrasaMaktab } from '@/components/marketing/WhyChooseMadrasaMaktab';
 
 export default function Home() {
@@ -13,6 +16,9 @@ export default function Home() {
       <ClassOptions />
       <CoursesGrid />
       <WhyChooseMadrasaMaktab />
+      <FAQ />
+      <StuckInDeenBanner />
+      <LatestFromBlog />
     </div>
   );
 }
